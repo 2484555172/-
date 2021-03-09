5665c736820759cac5d3c62ede3fcb8513b59c33
2484555172 <79183048+2484555172@users.noreply.github.com>
2021-03-09 04:17:18 -0800

    Add files via upload

--- test.c
+++ test.c
@@ -2,24 +2,25 @@
 
 #include <stdio.h>
 
+#include <string.h>
 
+//计算两数的和
+int Add(int a, int b)   //定义一个函数
+{
+	int c = 0;
+	c = a + b;
+	return c;
+}
 
 int main(void)
 {
 
-	for (int i = 0; i < 10; i++)
-	{
-		for (int j = 0; j < 10-1 - i; j++)
-		{
-			printf(" ");
-		}
-		for (int j = 0; j < 2 * i + 1; j++)
-		{
-			printf("*");
-		}
-		printf("\n");
-	}
-
+	int i = 0;
+	int j = 0;
+	int sum = 0;
+	scanf("%d%d", &i, &j);
+	sum = Add(i, j);
+	printf("%d", sum);
 	return 0;
 }
 
@@ -30,27 +31,63 @@ int main(void)
 
 
 
+
+
+//矩阵输入与打印
+//#define N  3
+//#define M  3
+
 //int main(void)
 //{
-//
-//
-//		printf("━━┮ ┲┳ ┍┒┎┲┒ ╭╮\n");
-//		printf("┍┒┃ ┕╄ ┖┛┞┺┚    \ /\n");
-//		printf("┖┛┚＇┃ヽ ┗━┚ ﹫\n");
-//		printf(" _  _              \n");
-//		printf("╭┘└┘└╮\n");
-//		printf("└┐．．┌┘────╮\n");
-//		printf("╭┴──┤          ├╮\n");
-//		printf("│ｏ　ｏ│          │ ●\n");
-//		printf("╰─┬─╯          │\n");
-//		printf("    HAPPY  NEW    YEAR\n"); 
-//	
-//
+//	int arr[N][M] = { 0 };
+//	for (int i = 0; i < N; i++)
+//	{
+//		for (int j = 0; j < M; j++)
+//		{
+//			scanf("%d", &arr[i][j]);
+//		}
+//	}
+//	printf("\n");
+//	for (int i = 0; i < N; i++)
+//	{
+//		for (int j = 0; j < M; j++)
+//		{
+//			printf("%d ", arr[j][i]);
+//		}
+//		printf("\n");
+//	}
 //	return 0;
 //}
 
 
+//#define m 100
 
-
-
+//int main(void)
+//{
+//	for (int i = 0; i < m; i++)
+//	{		
+//		for (int j = 0; j < m - i - 1; j++)
+//		{
+//			printf(" ");
+//		}
+//		for (int j = 0; j < 2*i+1; j++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	for (int i = 0; i < m-1; i++)
+//	{
+//		for (int j = 0; j < i+1; j++)
+//		{
+//			printf(" ");
+//		}
+//		for (int j = 0; j < 2 * m - 3 - 2 * i; j++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
 
