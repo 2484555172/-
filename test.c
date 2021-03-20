6bb53c6559cd4da1ce1316059668099098daa4a4
2484555172 <79183048+2484555172@users.noreply.github.com>
2021-03-20 04:40:41 -0800

    Add files via upload

--- test.c
+++ test.c
@@ -2,59 +2,196 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
-int main(void)
-{
-	return 0;
-}
+//输入一行电报文字，将字母变成其下一字母（如’a’变成’b’……’z’变成’ａ’其它字符不变）。
 
-//输入一个字符串,数出其中的字母的个数
-int main(void)
-{
-	char arr[100];
-	int count = 0;
-	gets(arr);
-	for (int i = 0; i < strlen(arr); i++)
-	{
-		if (arr[i] >= 'A'&&arr[i] <= 'z')
-		{
-			count++;
-		}
-	}
-	printf("%d", count);
-	return 0;
-}
-
-
-
-//输入一串字符,将其中的大写变成小写
-//若不为大写则原样输出
 //int main(void)
 //{
-//	char a[100];
-//	gets(a);
-//	for (int i = 0; i < strlen(a); i++)
+//	char arr[100];
+//	gets(arr);
+//	for (int i = 0; i < strlen(arr); i++)
 //	{
-//		if (a[i] >= 'A'&&a[i] <= 'Z')
-//			a[i] += 32;
+//		if (isalpha(arr[i]))
+//		{
+//			if (arr[i] == 'z')
+//			{
+//				arr[i] = 'a';
+//			}
+//			else
+//			{
+//				arr[i] += 1;
+//			}
+//		}
 //	}
-//	puts(a);
+//	puts(arr);
+//	return 0;
+//}
+
+//int main(void)
+//{
+//	int i;
+//	char array[80];
+//	while (gets(array))
+//	{
+//		for (i = 0; i < strlen(array); i++)
+//		{
+//			if (array[i] != 32)
+//				putchar(array[i]);
+//		}
+//		putchar('\n');
+//	}
 //	return 0;
 //}
 
 
 //int main(void)
 //{
-//	int arr[10] = { 1,2,9,4,3,7,9,4,5,3 };
-//	for (int i = 0; i < 10; i++)
+//	char arr[100];
+//	int alpha, brank, num, other;
+//	alpha = 0; brank = 0;
+//	num = 0; other = 0;
+//	gets(arr);
+//	for (int i = 0; i < strlen(arr); i++)
 //	{
-//		for (int j = 1+i; j < 10; j++)
+//		if (isalpha(arr[i]))  //判断字母
 //		{
-//			if (arr[i] == arr[j])
-//			{
-//				printf("%d ", arr[i]);
-//			}
+//			alpha++;
+//		}
+//		else if (arr[i] == ' ')  //判断空格
+//		{
+//			brank++;
+//		}
+//		else if (isalnum(arr[i]))   //判断数字
+//		{
+//			num++;
+//		}
+//		else
+//		{
+//			other++;
 //		}
 //	}
+//	printf("%d\n", alpha);
+//	printf("%d\n", brank);
+//	printf("%d\n", num);
+//	printf("%d\n", other);
+//	return 0;
+//}
+
+//求1+2!+3!+4!+…+30!
+//科学计数法，保留两位小数
+
+//double fact(int k)
+//{
+//	if (1 == k)
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return k * fact(k - 1);
+//	}
+//}
+//
+//
+//int main(void)
+//{
+//	int n;
+//	double sum = 0;
+//	for (int i = 1; i <= 30; i++)
+//	{
+//		sum += fact(i);
+//	}
+//	printf("%0.2e", sum);
+//	return 0;
+//}
+
+
+//输入一个正数x和一个正整数n，求下列算式的值。要求定义两个调用函数：
+//fact(n)计算n的阶乘；mypow(x, n)计算x的n次幂（即xn）
+//两个函数的返回值类型是double。
+//x - x2 / 2!+ x3 / 3!+ ... + (-1)n - 1xn / n!
+//×输出保留4位小数。
+
+//double fact(int k)
+//{
+//	if (1 == k)
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return k * fact(k - 1);
+//	}
+//}
+//
+//double mypow(double x, int n)
+//{
+//	if (1 == n)
+//	{
+//		return x;
+//	}
+//	else
+//	{
+//		return x*mypow(x, n - 1);
+//	}
+//}
+//
+//int main(void)
+//{
+//	double x; int n;
+//	double sum1, sum2, sum;
+//	sum1 = 0; sum2 = 0; sum = 0;
+//	scanf("%lf%d", &x, &n);
+//	for (int i = 1; i <= n; i++)
+//	{
+//		if (i % 2 == 0)
+//		{
+//			sum1 += mypow(x, i) / fact(i);
+//		}
+//		else
+//		{
+//			sum2 += mypow(x, i) / fact(i);
+//		}
+//	}
+//	sum = sum2 - sum1;
+//	printf("%0.4lf", sum);
+//	return 0;
+//}
+
+
+
+
+
+
+//编写程序，输入一个正整数n，求下列算式的
+//要求定义和调用函数fact(k)计算k的阶乘
+//函数返回值的类型是double。
+//1+1/2+ .... +1/n!
+
+//double fact(int k)
+//{
+//	if (1 == k)
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return k * fact(k - 1);
+//	}
+//}
+//
+//int main(void)
+//{
+//	int n;
+//	double ret = 0;
+//	double sum = 0;
+//	scanf("%d", &n);
+//	for (int i = 1; i <= n; i++)
+//	{
+//		sum += 1 / fact(i);
+//	}
+//	
+//	printf("sum=%0.5lf", sum);
 //	return 0;
 //}
